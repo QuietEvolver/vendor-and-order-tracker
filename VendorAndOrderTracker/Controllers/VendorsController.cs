c33d4d0627f66bd291e9939af07289793d6552e5
Vera Weikel <vera.weikel@gmail.com>
2023-03-03 14:17:31 -0800

    Add create() to vendor controllers.

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -19,5 +19,27 @@ namespace VendorAndOrderTracker.Controllers
     {
       return View();
     }
+
+    // create&process submissions from this form
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
+
+// This one creates new Orders within a given Vendor, not new vendors:
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create( int vendorId, string orderDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderDescription);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("Orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
