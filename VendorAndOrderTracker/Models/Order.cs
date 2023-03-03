1cdfc2fa754aa8cdf0fd4266b52b2c3c49925412
Vera Weikel <vera.weikel@gmail.com>
2023-03-03 10:36:05 -0800

    Round out Order model functions.

---
+++ VendorAndOrderTracker/Models/Order.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace VendorAndOrderTracker.Models
+{
+  public class Order
+  {
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
+
+    public Order(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
