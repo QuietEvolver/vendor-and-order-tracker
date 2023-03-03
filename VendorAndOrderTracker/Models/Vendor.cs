b6fa5c0649fff3049d60a64c388445609e06412a
Vera Weikel <vera.weikel@gmail.com>
2023-03-03 12:41:06 -0800

    Update views and model for Vendor.

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -6,8 +6,8 @@ namespace VendorAndOrderTracker.Models
   {
     private static List<Vendor> _instances = new List<Vendor>{};
     public string Name { get; set; }
-    public List<Order> Orders { get; set; }  
     public int Id { get; }
+    public List<Order> Orders { get; set; }  
     public Vendor(string vendorName)
     {
       Name = vendorName;
