68b65d2b343995676b35da9fc46c11f1713e6281
Vera Weikel <vera.weikel@gmail.com>
2023-03-03 12:01:13 -0800

    Add Vendor controllers for splash display and new page.

---
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,22 @@
+using System; 
+using Microsoft.AspNetCore.Mvc;
+using VendorAndOrderTracker.Models;
+
+namespace VendorAndOrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+  }
+}
