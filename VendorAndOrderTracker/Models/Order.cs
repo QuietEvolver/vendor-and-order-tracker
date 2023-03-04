5bec6627f89f6bbf7e02f7b3d3723f465be3c97a
Vera Weikel <vera.weikel@gmail.com>
2023-03-03 17:48:55 -0800

    Update order model.

--- VendorAndOrderTracker/Models/Order.cs
+++ VendorAndOrderTracker/Models/Order.cs
@@ -20,14 +20,14 @@ namespace VendorAndOrderTracker.Models
       return _instances;
     }
 
-    public static Order Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
-
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
