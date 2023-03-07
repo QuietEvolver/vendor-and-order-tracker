56ff17c45921fe10b65083d976562a25fa7a8c36
Vera Weikel <vera.weikel@gmail.com>
2023-03-07 09:21:58 -0800

    Update syntax.

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -6,7 +6,7 @@ namespace VendorAndOrderTracker.Models
   {
     private static List<Vendor> _instances = new List<Vendor>{};
     public string Name { get; set; }
-    public int Id { get; }
+    public int Id { get; set; }
     public List<Order> Orders { get; set; }  
     public Vendor(string vendorName)
     {
